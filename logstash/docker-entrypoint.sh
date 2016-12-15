26d08b31caff2cf369b8596062a4ae0c12795ee4
root <root@ip-10-0-1-25.ec2.internal>
2016-12-15 09:41:23 +0000

    changes in configurations

---
+++ logstash/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+        set -- logstash "$@"
+fi
+
+# Run as user "logstash" if the command is "logstash"
+# allow the container to be started with `--user`
+if [ "$1" = 'logstash' -a "$(id -u)" = '0' ]; then
+        chown -R logstash:logstash /usr/share/logstash/data
+        set -- gosu logstash "$@"
+fi
+
+exec "$@"
+
